8eace36754d9b8671e904b7c74e3a6035d72233a
Sushil Kumar Mahato <suloksushil@gmail.com>
2020-08-26 10:16:54 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,8 @@
+RAWDICT = {'team':'group of players playing for one specific region or satisfaction',
+           'supreme': 'High in Command', 'Raw':'Not cooked or flesh/Uncooked',
+           'Deejavu': 'Something which you feel happen before in your dreams or previous birth'}
+# For use to enter the string
+key = str(input())
+# For searching the value with the help of a key
+print(RAWDICT[key])
+
